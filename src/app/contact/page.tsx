625782ea626b47ea4867c56b6451b5bdcb774dd2
Toby Killen <itoby24@gmail.com>
2024-12-31 23:36:45 +0000

    Removing unused vars

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,4 @@
 'use client';
-import Link from "next/link";
-import Image from "next/image";
-import Myself from "./../../public/me_1.jpg";
 import Background from "../components/background";
 import BackgroundImage from './../../../public/background.jpg';
 import { useState } from "react";
