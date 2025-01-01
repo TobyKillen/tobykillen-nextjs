43bcd846b668182bb6959f367ed55212dc1cd38b
Toby Killen <itoby24@gmail.com>
2025-01-01 23:23:49 +0000

    Adding Dynamic Footer

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
       <div>
         <footer className="footer items-center p-6 bg-black text-white">
           <div className="container mx-auto flex flex-col md:flex-row justify-between items-center gap-4">
-            <span className="text-lg font-semibold">© 2024 TOBYKILLEN.COM</span>
+            <span className="text-lg font-semibold">© {new Date().getFullYear()} Toby Killen</span>
             <span className="text-sm">All rights reserved.</span>
           </div>
         </footer>
